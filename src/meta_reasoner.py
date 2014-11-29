83feabbd7129946f3ce2dc32e68ca7d2a568190d
Shiqi Zhang <shiqi.zhang6@gmail.com>
2014-11-29 14:09:37 -0600

    added a POMDP parser

--- src/meta_reasoner.py
+++ src/meta_reasoner.py
@@ -1,26 +1,38 @@
 
 class Reasoner:
 
-    # -------------------------------------->>
+    #####################################################################
     def __init__(self, 
-                 mapfile = '/home/szhang/projects/2015_meta_reasoning_pomdp/meta-reasoning/maps/4x3.95.map'
+                 mapfile = '/home/szhang/projects/2015_meta_reasoning_pomdp/meta-reasoning/maps/4x3.95.map',
+                 mapfile = '/home/szhang/projects/2015_meta_reasoning_pomdp/meta-reasoning/models/4x3.95.POMDP'
                  ):
 
         self.gridmap = []
         self.mapfile = mapfile
-        self.read_map()
+        self.gridmap = self.read_map(self.mapfile)
 
-        # as a result, probability to other nearby (three) positions is 0.02
-        self.prob_motion_forward = 0.9
-        self.prob_motion_stay = 0.04
+        # motion model
+        self.motion = {'forward':0.9, 'stay':0.06, 'left':0.02, 'right':0.02}
 
-        # TODO
+        # observation model: left, front, right
+        self.obs = {'ppp':0.91, \
+                    'npp':0.02, 'pnp':0.02, 'ppn':0.02, \
+                    'nnp':0.01, 'npn':0.01, 'pnn':0.01}
         
-    # -------------------------------------->>
-    def read_map(self):
+        self.model = self.create_model(self.gridmap, self.motion, self.obs)
+
+        self.write_to_file(self.model, modelfile)
+
+    #####################################################################
+    def create_model(self, gridmap, motion, obs)
+
         
-        mapfile = self.mapfile
+
+    #####################################################################
+    def read_map(self, mapfile):
+        
         f = open(mapfile, 'r')
+        gridmap = []
 
         while True:
 
@@ -29,24 +41,22 @@ class Reasoner:
             if l == '':
                 break;
             else:
-                self.gridmap.append(l[1:-2])
+                gridmap.append(l[1:-2])
 
-        self.gridmap = self.gridmap[1:-1]
+        gridmap = gridmap[1:-1]
 
+        return gridmap
 
-
-    # -------------------------------------->>
+    #####################################################################
     def get_map_size(self):
 
         return len(self.gridmap), len(self.gridmap[0])
 
 
-
-# -------------------------------------->>
-# -------------------------------------->>
-
+#####################################################################
+#####################################################################
 if __name__ == "__main__":
 
     reasoner = Reasoner()
 
-
+    return True
